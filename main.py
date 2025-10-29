af6727bff25157c6fbe35bbddc7e5582ada95b0b
Charlie <charlies.retro.computing@gmail.com>
2025-10-29 19:46:12 -0400

    Improve the script

--- main.py
+++ main.py
@@ -4,44 +4,54 @@ Exits with code 0 if successful, 1 otherwise.
 """
 
 import sys
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet, Tag
 from playwright.sync_api import sync_playwright
+from typing import Final, Union
 
 
-def _print(message: str) -> None:
-    print(message, flush=True)
+buttons_selector: Final[str] = "div.dc-page-available-downloads-hero-button > button"
+driver_page: Final[str] = "https://www.intel.com/content/www/us/en/download/785597/intel-arc-iris-xe-graphics-windows.html"  # noqa: E501
 
 
-def get_link() -> str:
+def get_link() -> Union[str, None]:
+    download_link: Union[str, None] = None
+
     with sync_playwright() as playwright:
-        chromium = playwright.chromium.launch(headless=True)
-        page = chromium.new_page()
-        page.goto(
-            "https://www.intel.com/content/www/us/en/download/785597/"
-            "intel-arc-iris-xe-graphics-windows.html"
-        )
-        html = page.content()
-        chromium.close()
+        with playwright.chromium.launch(headless=True) as chromium:
+            with chromium.new_page() as page:
+                page.goto(driver_page)
+                page.wait_for_selector(
+                    selector=buttons_selector,
+                    state="attached",
+                    timeout=15000
+                )
+                html: str = page.content()
 
-    soup = BeautifulSoup(html, 'html.parser')
+    soup: BeautifulSoup = BeautifulSoup(html, "html.parser")
 
-    download_link = soup.select(
-        'div.dc-page-available-downloads-hero-button > button'
-    )[0]['data-href']
+    buttons: ResultSet[Tag] = soup.select(buttons_selector)
 
-    return str(download_link)
+    if buttons:
+        download_button: Tag = buttons[0]
+        raw_download_link = download_button.get("data-href")
+        download_link = raw_download_link if isinstance(raw_download_link, str) else None
+
+        if download_link is not None:
+            download_link = download_link.strip()
+
+    return download_link
 
 
-if __name__ == "__main__":
-    try:
+try:
+    if __name__ == "__main__":
         link = get_link()
 
-        if link:
-            _print(link)
+        if link is not None and link != "":
+            print(link, flush=True)
             sys.exit(0)
         else:
-            sys.exit(1)
-    except Exception:
-        sys.exit(1)
-else:
+            raise Exception("No link found")
+    else:
+        raise Exception("This is not intended to be run as a module.")
+except Exception:
     sys.exit(1)
